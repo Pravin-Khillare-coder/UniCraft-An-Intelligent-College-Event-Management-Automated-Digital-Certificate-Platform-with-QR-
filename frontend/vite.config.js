5e4688c8d8acf92cc33c05d736357ea036349dea
Pravin-Khillare-coder <pravinkhillare1111@gmail.com>
2026-08-11 19:05:43 +0530

    Configure base ./ in vite.config.js to ensure live GitHub Pages deployment loads updated assets

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,9 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.GITHUB_REPOSITORY 
-    ? `/${process.env.GITHUB_REPOSITORY.split('/')[1]}/` 
-    : './',
+  base: './',
   plugins: [react()],
   server: {
     host: true,
